8be18517894518f73e955539dbcb1c404bc0c40c
Williams Balogun <williamsdev.official@gmail.com>
2022-12-28 23:07:46 +0100

    built cocktail drinks info app using cocktail API

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        debitis molestias facere, atque aliquid ex nulla molestiae sed deleniti
+        alias eligendi ea veniam. Omnis ducimus earum incidunt odio illo harum
+        et doloribus quasi animi nemo, quam expedita, enim ea vitae?
+      </p>
+    </section>
+  )
+}
+
+export default About
